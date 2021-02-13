2f4299226d9a842b3a7fc853d57a2b07349de695
Mxum <74014598+datetam@users.noreply.github.com>
2021-02-13 23:37:37 +0300

    Delete figma directory

--- figma/scripts/script.js
+++
@@ -1,9 +0,0 @@
-function ModalWindow (id, toggle) {
-    if (toggle) {
-        document.getElementById(id).classList.remove("window_hidden");
-        document.getElementById(id).classList.toggle("window_active");
-    } else { 
-        document.getElementById(id).classList.remove("window_active"); 
-        document.getElementById(id).classList.add("window_hidden");
-    }
-}
